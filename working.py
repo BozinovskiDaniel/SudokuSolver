16cfecd34d107e5cf39aec30289ec7bc316733f1
Daniel Bozinovski <BozinovskiDaniel@hotmail.com>
2019-12-20 13:49:09 +1100

    latest v of working board

--- working.py
+++ working.py
@@ -10,3 +10,40 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+# Function to pick empty square
+# Can just loop through, and try all numbers
+# Function to find if num is valid
+def display_board(given):
+
+    for i in range(len(given)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+
+        for j in range(len(given[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+                
+            if j == 8:
+                print(given[i][j])
+            else:
+                print(str(given[i][j]) + " ", end="")
+
+
+# Find an empty square, which returns the position
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j) # row, col
+
+
+# Check if the board is valid
+def is_valid(board, num, pos):
+
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+#display_board(board)
